ebc34187d099604eccd14ed6eb5cfec7e032b158
GabrielBrunatti <55466917+GabrielBrunatti@users.noreply.github.com>
2020-04-14 20:57:40 -0300

    Models

---
+++ Models/TbCurriculo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AD___2FA.Models
+{
+    [Table("tbCurriculo")]
+    public partial class TbCurriculo
+    {
+        public TbCurriculo()
+        {
+            TbProfessor = new HashSet<TbProfessor>();
+        }
+
+        [Key]
+        [Column("idCurriculo")]
+        public int IdCurriculo { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Graduacao { get; set; }
+        [Column("Anos_Experiencia")]
+        public int AnosExperiencia { get; set; }
+        [Required]
+        [Column("Cursos_Realizados")]
+        [StringLength(300)]
+        public string CursosRealizados { get; set; }
+
+        [InverseProperty("IdCurriculoNavigation")]
+        public virtual ICollection<TbProfessor> TbProfessor { get; set; }
+    }
+}
---
+++ Models/TbVendas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AD___2FA.Models
+{
+    [Table("tbVendas")]
+    public partial class TbVendas
+    {
+        [Key]
+        [Column("idVendas")]
+        public int IdVendas { get; set; }
+        [Column("idCursos")]
+        public int? IdCursos { get; set; }
+        [Column("idCliente")]
+        public int? IdCliente { get; set; }
+        [Column("Data_Compra", TypeName = "datetime")]
+        public DateTime DataCompra { get; set; }
+
+        [ForeignKey(nameof(IdCliente))]
+        [InverseProperty(nameof(TbCliente.TbVendas))]
+        public virtual TbCliente IdClienteNavigation { get; set; }
+        [ForeignKey(nameof(IdCursos))]
+        [InverseProperty(nameof(TbCursos.TbVendas))]
+        public virtual TbCursos IdCursosNavigation { get; set; }
+    }
+}
